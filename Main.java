6278cafc28d353add2a44eaa0c6783007173128d
Tamara <tamara.elheet018@gmail.com>
2023-12-03 02:47:41 +0300

    Adding Main.java by user1

---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, I'm Tamara.");
+        System.out.println("This is my fiirst DevOps assignment");
+    }
+}
