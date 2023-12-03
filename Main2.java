a3ecfdcf93e9106d37270afa751f83fa6d9f6415
Tamara <tamara.elheet018@gmail.com>
2023-12-03 03:29:11 +0300

    Adding Main2.java by user2

---
+++ Main2.java
@@ -0,0 +1,6 @@
+public class Main2 {
+    public static void main(String[] args) {
+        System.out.println("Hello again!, still me");
+        System.out.println("The DevOps assignment is going well so far!");
+    }
+}
